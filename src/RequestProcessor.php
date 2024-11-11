dc94a0b12b1f3425c91a52475778809784771c66
tig3306 <pig87800@proton.me>
2024-11-11 22:53:37 +0800

    v1.3

--- src/RequestProcessor.php
+++ src/RequestProcessor.php
@@ -139,7 +139,8 @@ class RequestProcessor
                                 if (!empty($info["file_validation_url_http"]??'')){
                                      $path = parse_url($info["file_validation_url_http"], PHP_URL_PATH);
                                      $filename = basename($path);
-                                     echo $options->targetPath. dirname($path);
+
+                                     echo 'mkdir path'. $options->targetPath. dirname($path);
                                     mkdir( $options->targetPath. dirname($path),0777,true);
                                     file_put_contents( $options->targetPath.$path,implode("\n",$info["file_validation_content"]));
                                 }
